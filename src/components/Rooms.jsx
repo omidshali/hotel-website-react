fabda8691b5598784fe0eda0cee9fce7843cf100
omid <o.shali01@gmail.com>
2024-02-26 18:40:59 +0330

    Added rooms component

---
+++ src/components/Rooms.jsx
@@ -0,0 +1,32 @@
+const Rooms = () => {
+  return (
+    <div className="max-w-[1400px] h-[500px] bg-blue-100 mx-auto my-20 pt-16 lg:mb-[20%] md:mb-[35%] px-4 grid lg:grid-cols-3 gap-4">
+      <div className="relative lg:top-20 col-span-2 lg:col-span-1">
+        <h3 className="font-bold text-2xl">Fine Interior Rooms</h3>
+        <p className="pt-4">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error ipsam
+          rerum iusto excepturi similique minus?
+        </p>
+      </div>
+      <div className="col-span-2 grid grid-cols-2 gap-2">
+        <img
+          className="object-cover w-full h-full"
+          src="https://images.unsplash.com/photo-1602002418082-a4443e081dd1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80"
+          alt="/"
+        />
+        <img
+          className="object-cover w-full h-full row-span-2"
+          src="https://images.unsplash.com/photo-1512918728675-ed5a9ecdebfd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80"
+          alt="/"
+        />
+        <img
+          className="object-cover w-full h-full"
+          src="https://images.unsplash.com/photo-1582719478250-c89cae4dc85b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80"
+          alt="/"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Rooms;
